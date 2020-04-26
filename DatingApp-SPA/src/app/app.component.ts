b4acdefb69b57a2b2f707e1113ca8b223f2513b5
awais4you <awais4you@gmail.com>
2020-04-26 13:44:15 +0500

    End of Section 4

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dating Application 123 -SPA';
+  title = 'Dating App';
 }
