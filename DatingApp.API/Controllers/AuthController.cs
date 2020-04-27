1c3dc723da52e28dd0e79a86c86247bb9f3218b5
awais4you <awais4you@gmail.com>
2020-04-27 13:28:57 +0500

    End of Section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -44,7 +44,7 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-        {
+        {  
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -72,6 +72,7 @@ namespace DatingApp.API.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
             
             return Ok( new { token = tokenHandler.WriteToken(token) });
+            
         }
     }
 }
